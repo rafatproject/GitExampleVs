51680a2a7a8898fe1b53d70292f25850a98308b6
leylama <leylama@JUPITER02>
2026-09-13 19:34:12 +0400

    dv commit

--- GitExampleVs/Program.cs
+++ GitExampleVs/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! from test");
+            Console.WriteLine("Hello, World! from tet");
          
         }
     }
